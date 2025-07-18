33ae50eb8eb6b30cad9b57c6711687575c616a1e
Needium66 <jide.efunkunle8@gmail.com>
2025-07-18 02:54:32 -0500

    adds helper script for api gateway integration with microservices in gcp

--- Google-APIGateway-FusionAuth/google_apigateway_fusionauth.py
+++ Google-APIGateway-FusionAuth/google_apigateway_fusionauth.py
@@ -280,41 +280,7 @@ paths:
 
 
 google-cloud-firestore
-6. scripts/package_functions.sh (Helper Script for Cloud Function Zips)
 
-Bash
-
-#!/bin/bash
-set -e
-
-# This script creates zip files for Cloud Functions with their dependencies.
-
-# Auth Function
-echo "Packaging auth_function..."
-cd functions/auth_function
-pip install -r requirements.txt -t .
-zip -r ../auth_function.zip .
-rm -rf lib python *.dist-info # Clean up downloaded packages
-cd ../..
-
-# Payment Processor Function
-echo "Packaging payment_processor_function..."
-cd functions/payment_processor_function
-pip install -r requirements.txt -t .
-zip -r ../payment_processor_function.zip .
-rm -rf lib python *.dist-info
-cd ../..
-
-# Telemedicine Processor Function
-echo "Packaging telemedicine_processor_function..."
-cd functions/telemedicine_processor_function
-pip install -r requirements.txt -t .
-zip -r ../telemedicine_processor_function.zip .
-rm -rf lib python *.dist-info
-cd ../..
-
-echo "Cloud Function packaging complete."
-Make this script executable: chmod +x scripts/package_functions.sh
 
 Implementation Steps:
 
